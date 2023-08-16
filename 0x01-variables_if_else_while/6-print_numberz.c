8f789078fe9971941b1a36788ad37a71579655f1
Amidu <mualamuhammed1998@gmail.com>
2023-08-16 07:24:10 -0700

    here

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-/**
- * main - Entry point
- * Description: 'prints numbers in reverse'
- * Return: always 0
- */
-int main(void)
-{
-	int n = 122;
 
-	while (n >= 97)
-	{
-		putchar(n);
-		n--;
-	}
-	putchar('\n');
-	return (0);
-}
